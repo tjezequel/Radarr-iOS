151ff5373c6f0718d837b8d5ad0350d606b44871
Thomas <thomas@2a01cb08868f7400145be7652558130a.ipv6.abo.wanadoo.fr>
2018-11-30 21:55:38 +0100

    Initial Commit

---
+++ Unraid-Notifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unraid-Notifier
+//
+//  Created by Thomas on 30/11/2018.
+//  Copyright © 2018 Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Unraid-NotifierTests/Unraid_NotifierTests.swift
@@ -0,0 +1,34 @@
+//
+//  Unraid_NotifierTests.swift
+//  Unraid-NotifierTests
+//
+//  Created by Thomas on 30/11/2018.
+//  Copyright © 2018 Thomas. All rights reserved.
+//
+
+import XCTest
+@testable import Unraid_Notifier
+
+class Unraid_NotifierTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
